9b5ddeaeddc59eaa88e0537dcd20f13c6f7b9e2b
mansonp <mansonp@pdx.edu>
2023-03-21 11:39:46 -0700

    Update with data

---
+++ scripts/summarystats.R
@@ -0,0 +1,22 @@
+
+library(pacman)
+p_load(srvyr, gtsummary, sjmisc, haven, tidyverse)
+
+
+nsee2017 <- read_sav("data/nsee2017.sav")
+
+nsee.svy <- nsee2017 %>% as_survey_design(weight=Weight)
+
+nsee.svy %>% mutate(across(where(is.labelled), ~as_factor(.x) %>% fct_drop)) %>%
+   tbl_svysummary(by=gw_belief)
+
+nsee2017 %>%
+   select(gw_belief, Weight) %>%
+   mutate(across(where(is.labelled), ~as_factor(.x) %>% fct_drop)) %>%
+   as_survey_design(weight=Weight) %>%
+   tbl_svysummary(include=gw_belief)
+
+nsee2017 %>%
+   select(gw_belief, Weight) %>%
+   mutate(across(where(is.labelled), ~as_factor(.x) %>% fct_drop)) %>%
+   tbl_summary()
